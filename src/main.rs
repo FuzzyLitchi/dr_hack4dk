40f25a8b9825fdb5b1a98b533ca9ffbe4b17ff72
Emile Yvan Boutet Livoff <blemille@hotmail.com>
2017-09-30 13:53:40 +0200

    add searching in a bad bad way

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[allow(unused_extern_crates)]
 extern crate serde;
 extern crate serde_json;
 extern crate chrono;
+extern crate tantivy;
+extern crate tempdir;
 
+use chrono::NaiveDate;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use chrono::NaiveDate;
+use tempdir::TempDir;
+use tantivy::Index;
+use tantivy::schema::*;
+use tantivy::collector::TopCollector;
+use tantivy::query::QueryParser;
 
 #[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct RadioProgram {
     allText: String,
     date: NaiveDate,
@@ -40,5 +48,42 @@ fn main() {
                 panic!("Failed to read line.");
             },
         };
+    }
+
+    let index_dir = TempDir::new("dr_index").unwrap();
+    let index_path = index_dir.path();
+    let mut schema_builder = SchemaBuilder::default();
+
+    let title = schema_builder.add_text_field("title", TEXT | STORED);
+    let all_text = schema_builder.add_text_field("allText", TEXT | STORED);
+
+    let schema = schema_builder.build();
+    let index = Index::create(index_path, schema.clone()).unwrap();
+
+    let mut index_writer = index.writer(50_000_000).unwrap();
+    for v in values {
+        let mut document = Document::default();
+        document.add_text(title, &v.title);
+        document.add_text(all_text, &v.allText);
+
+        index_writer.add_document(document);
+    }
+
+    index_writer.commit().unwrap();
+    index.load_searchers().unwrap();
+    let searcher = index.searcher();
+
+    let query_parser = QueryParser::new(index.schema(), vec![title, all_text]);
+
+    let query = query_parser.parse_query("Hej mor").unwrap();
+
+    let mut top_collector = TopCollector::with_limit(10);
+
+    searcher.search(&*query, &mut top_collector).unwrap();
+    let doc_addresses = top_collector.docs();
+
+    for doc_address in doc_addresses {
+       let retrieved_doc = searcher.doc(&doc_address).unwrap();
+       println!("{}", schema.to_json(&retrieved_doc));
     }
 }
